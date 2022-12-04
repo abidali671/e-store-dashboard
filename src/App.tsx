fdc0822f4b76c18b9d39239190f31dd53ddcfa87
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2022-12-04 05:21:03 -0800

    sign in UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Navbar from './components/Navbar.jsx'
 function App() {
-	return <>
-	hello i m wahab </>;
+	return (
+		<>
+		<Navbar />
+			<Routes>
+				<Route path='/login' element={<Login />} />
+				
+			</Routes>
+			</>
+	);
 }
 
 export default App;
