651462e207961d1d3d31fe8805d74a73962b1940
abidali671 <abid.saeed.ali92@gmail.com>
2022-12-04 21:51:45 +0500

    refactor navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,23 +1,13 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import { Box, Typography } from '@mui/material';
+import * as styles from './navbar.styles';
+import * as types from './navbar.types';
 
-function Navbar() {
+const Navbar: types.ComponentT = () => {
 	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position='static'>
-				<Toolbar>
-					<Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-						News
-					</Typography>
-					<Button color='inherit'>Login</Button>
-				</Toolbar>
-			</AppBar>
+		<Box sx={styles.root}>
+			<Typography variant='h4'>EKKA</Typography>
 		</Box>
 	);
-}
+};
 
 export default Navbar;
