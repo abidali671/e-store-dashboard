febd6ba67fd8321e538c532de0b518176bb10ef6
abidali671 <abid.saeed.ali92@gmail.com>
2022-12-04 21:51:45 +0500

    refactor navbar

---
+++ src/components/navbar/navbar.styles.ts
@@ -0,0 +1,11 @@
+import { SxProps, Theme } from '@mui/material';
+
+export const root: SxProps<Theme> = {
+	width: '100%',
+	height: '80px',
+	background: '#FFF',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'flex-start',
+	px: '40px',
+};
