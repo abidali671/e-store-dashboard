beb745c2ab49bdbccb553e3cd1ab1c6d10f739dc
abidali671 <abid.saeed.ali92@gmail.com>
2022-12-04 21:58:22 +0500

    update code base

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import { AuthContainer } from './components';
+import Login from './pages/auth/login';
 
 const App: React.FC = () => {
 	return (
 		<Routes>
-			<Route
-				path='/login'
-				element={
-					<AuthContainer>
-						<Login />
-					</AuthContainer>
-				}
-			/>
-			<Route
-				path='/register'
-				element={
-					<AuthContainer>
-						<Login />
-					</AuthContainer>
-				}
-			/>
+			<Route path='/login' element={<Login />} />
 		</Routes>
 	);
 };
