738782e0e4c4db39cbeac84090fc34fece25a0d5
abidali671 <abid.saeed.ali92@gmail.com>
2022-12-06 13:27:12 +0500

    default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/auth/login';
 
 const App: React.FC = () => {
 	return (
 		<Routes>
 			<Route path='/login' element={<Login />} />
+			<Route path='*' element={<Navigate to='/login' />} />
 		</Routes>
 	);
 };
