85d5d7d16b564bb589b0b12cfac50ae4b49c749f
abidali671 <abid.saeed.ali92@gmail.com>
2022-12-06 14:17:08 +0500

    alias for assests

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 module.exports = {
 	webpack: {
 		alias: {
+			'@assests/*': path.resolve(__dirname, 'src/assests/*'),
+			'@assests': path.resolve(__dirname, 'src/assests'),
 			'@components': path.resolve(__dirname, 'src/components'),
 			'@components/*': path.resolve(__dirname, 'src/components/*'),
 		},
