cf97012a2b04936ad3c240d5d57a44b7f726357d
abidali671 <abid.saeed.ali92@gmail.com>
2023-01-21 22:18:27 +0500

    update container

--- src/components/auth_container/index.tsx
+++ src/components/auth_container/index.tsx
@@ -7,18 +7,16 @@ import SignInImg from '@assests/super_sale.png';
 
 const AuthContainer: types.ComponentT = ({ children }: types.PropsT) => {
 	return (
-		<Box>
+		<Box sx={styles.root}>
 			<Navbar />
-			<Box sx={{ flexGrow: 1, justifyContent: 'center', alignItem: 'center' }}>
-				<Grid container columns={12}>
-					<Grid item lg={6} md={6} xs={6}>
-						<Box component='img' src={SignInImg} sx={styles.image} />
-					</Grid>
-					<Grid item lg={6} md={6} sm={12} flexGrow={1}>
-						{children}
-					</Grid>
+			<Grid container columns={12}>
+				<Grid item xs={6}>
+					<Box component='img' src={SignInImg} sx={styles.coverImg} />
 				</Grid>
-			</Box>
+				<Grid item xs={6} flexGrow={1}>
+					{children}
+				</Grid>
+			</Grid>
 		</Box>
 	);
 };
