25715ba0795e1db71847567d2ac73db0515cc72e
abidali671 <abid.saeed.ali92@gmail.com>
2023-03-13 02:25:29 +0500

    update dashboard code structure

---
+++ src/pages/dashboard/dashboard.styles.ts
@@ -0,0 +1,15 @@
+import { SxProps, Theme } from '@mui/material';
+
+export const itemCard: SxProps<Theme> = {
+	p: 2,
+	border: '1px solid #D9D9D9',
+	borderRadius: '16px',
+	display: 'flex',
+	justifyContent: 'space-between',
+	alignItems: 'center',
+};
+
+export const cardTitle: SxProps<Theme> = {
+	color: 'black',
+	fontWeight: 'bold',
+};
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Container, View } from '@components';
+import { Container } from '@components';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { ViewDetails, ViewDetailsT } from '../../data/app.data';
+import Typography from '@mui/material/Typography';
+import { DenseTable, RowTable } from '../../components';
+import Box1 from '@components/box/Box1';
+import * as styles from './dashboard.styles';
+import _map from 'lodash/map';
 
 const Dashboard: React.FC = () => {
 	return (
 		<Container>
-			<View />
+			<Box sx={{ flexGrow: 1 }}>
+				<Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 6, sm: 6, md: 12 }}>
+					{_map(ViewDetails, (item: ViewDetailsT, index) => (
+						<Grid item xs={6} sm={3} md={3} key={index}>
+							<Box sx={styles.itemCard}>
+								<Box>
+									<Typography variant='h5' sx={styles.cardTitle}>
+										{item.title}
+									</Typography>
+									<Typography variant='caption'>{item.info}</Typography>
+								</Box>
+
+								{item?.icon && <item.icon />}
+							</Box>
+						</Grid>
+					))}
+					<Box1 />
+					{/* <DenseTable />
+					<RowTable /> */}
+				</Grid>
+			</Box>
 		</Container>
 	);
 };
