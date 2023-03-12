54b0ac8dd3e8242583db41a5175474ffa49a90f2
abidali671 <abid.saeed.ali92@gmail.com>
2023-03-13 02:03:09 +0500

    fix auth container cover img

--- src/components/auth_container/index.tsx
+++ src/components/auth_container/index.tsx
@@ -1,4 +1,4 @@
-import { Box, CardMedia, Grid } from '@mui/material';
+import { Box } from '@mui/material';
 import { Navbar } from '@components';
 import * as types from './auth_container.types';
 import * as styles from './auth_container.styles';
@@ -9,23 +9,12 @@ const AuthContainer: types.ComponentT = ({ children }: types.PropsT) => {
 	return (
 		<Box sx={styles.root}>
 			<Navbar isSearchBar={false} />
-			<div style={{ width: '100%' }}>
-				<Box
-					sx={{
-						display: 'flex',
-
-						justifyContent: { md: 'space-around', sm: 'center' },
-						alignItems: 'center',
-					}}
-				>
-					<Box sx={{ flex: 1, display: { md: 'block', xs: 'none', width: '100%' } }}>
-						<Box component='img' src={Rectangle} sx={styles.coverImg} />
-					</Box>
-					<Box mt={2} sx={{ flex: 1 }}>
-						{children}
-					</Box>
+			<Box sx={styles.contentWrapper}>
+				<Box sx={styles.imgWrapper}>
+					<Box component='img' src={Rectangle} sx={styles.coverImg} />
 				</Box>
-			</div>
+				<Box>{children}</Box>
+			</Box>
 		</Box>
 	);
 };
