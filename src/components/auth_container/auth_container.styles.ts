0bfc75940c66a1aac6a8f87a65f70261eac9ed1e
abidali671 <abid.saeed.ali92@gmail.com>
2023-03-13 02:03:09 +0500

    fix auth container cover img

--- src/components/auth_container/auth_container.styles.ts
+++ src/components/auth_container/auth_container.styles.ts
@@ -1,13 +1,30 @@
 import { SxProps, Theme } from '@mui/material';
 
 export const root: SxProps<Theme> = {
-	height: '100%',
+	height: '100vh',
 	display: 'grid',
-	gridTemplateRows: '60px auto',
+	gridTemplateRows: '60px 1fr',
 };
 
 export const coverImg: SxProps<Theme> = {
-	width: '70%',
-	display: { md: 'block', xs: 'none' },
+	width: '100%',
+	height: '100%',
+	position: 'absolute',
+	objectFit: 'cover',
 };
 
+export const imgWrapper: SxProps<Theme> = {
+	flex: 1,
+	display: { md: 'block', xs: 'none' },
+	width: '100%',
+	height: '100%',
+	background: 'red',
+	position: 'relative',
+};
+
+export const contentWrapper: SxProps<Theme> = {
+	justifyContent: { md: 'space-around', sm: 'center' },
+	alignItems: 'center',
+	display: 'grid',
+	gridTemplateColumns: '1fr 1fr',
+};
