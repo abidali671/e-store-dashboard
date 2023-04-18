1f5ff0306a21f435fc0ee63923261bd448598dbb
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2023-04-18 03:39:33 -0700

    fix style

--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -1,18 +1,19 @@
 import { Box, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import Shirt from '@assests/bag.png'
 import * as styles from './productCard.style'
-const ProductCard = () => {
+import useAxios from 'src/axios'
+const ProductCard = ({ title, price, image, key }) => {
     return (
-        <Box sx={styles.mainProduct}>
-            {Array(9).fill(<Box sx={styles.card}>
-                <Box component='img' src={Shirt} width='100%' />
-                <Box sx={styles.cardContent}>
-                    <Typography variant="body1" color='gray.400'>Glomy Hoodie</Typography>
-                    <Typography variant="body2" fontWeight='bold'>450$</Typography>
-                </Box>
-            </Box>)}
+
+        <Box sx={styles.card} key={key}>
+            <Box component='img' src={image} sx={{ height: '200px', width: '60%', objectFit: 'contain', borderRadius: '20px 20px 0px 0px' }} />
+            <Box sx={styles.cardContent}>
+                <Typography variant='body1' color='gray.400'>{title}</Typography>
+                <Typography variant='body2' fontWeight='bold'>{price + '$'}</Typography>
+            </Box>
         </Box>
+
     )
 }
 
