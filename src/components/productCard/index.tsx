d19e8c0a180038ddbe18f227cd59fada99901fde
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2023-04-25 14:31:38 -0700

    update

--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -3,14 +3,15 @@ import React, { useState } from 'react'
 import Shirt from '@assests/bag.png'
 import * as styles from './productCard.style'
 import useAxios from 'src/axios'
-const ProductCard = ({ title, price, image, key }) => {
+
+const ProductCard = ({ title, price }) => {
     return (
 
-        <Box sx={styles.card} key={key}>
-            <Box component='img' src={image} sx={{ height: '200px', width: '60%', objectFit: 'contain', borderRadius: '20px 20px 0px 0px' }} />
+        <Box sx={styles.card}>
+            <Box component='img' src={Shirt} sx={{ width: '100%', objectFit: 'contain', borderRadius: '20px 20px 0px 0px' }} />
             <Box sx={styles.cardContent}>
                 <Typography variant='body1' color='gray.400'>{title}</Typography>
-                <Typography variant='body2' fontWeight='bold'>{price + '$'}</Typography>
+                <Typography variant='body2' fontWeight='bold'>{price}</Typography>
             </Box>
         </Box>
 
