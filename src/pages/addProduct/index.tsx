4062f84821c130ac4a34ba9c2270b2fc84a80dd2
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2023-04-26 15:16:58 -0700

    add fields

--- src/pages/addProduct/index.tsx
+++ src/pages/addProduct/index.tsx
@@ -1,148 +1,180 @@
 import Shirt from '@assests/bag.png';
 import Container from '@components/container';
 import FormikTextField from '@components/formik_text_field';
-import { Box, Stack, Typography, Grid, TextField } from '@mui/material';
+import { Box, Stack, Typography, Grid, TextField, Checkbox } from '@mui/material';
 import React from 'react';
 
 const AddProduct = () => {
-	return (
-		<Container>
-			<Typography variant='h5' fontWeight='bold'>
-				Products
-			</Typography>
-			<Box sx={{ border: '1px solid', borderColor: 'gray.100', borderRadius: '20px' }}>
-				<Box sx={{ display: 'grid', p: 3, gap: 3, gridTemplateColumns: 'repeat(12,1fr)' }}>
-					<Box gridColumn={{ md: 'span 4', xs: 'span 12' }}>
-						<Box
-							sx={{
-								border: '1px solid',
-								borderColor: 'gray.100',
-								borderRadius: '20px',
-								aspectRatio: '4 / 3',
-								display: 'flex',
-								alignItems: 'center',
-								justifyContent: 'center',
-							}}
-						>
-							<Box>
-								<Typography variant='h2' fontWeight='bold' textAlign='center' color='gray.500'>
-									765 x 850
-								</Typography>
-								<Typography variant='body1' color='gray.100'>
-									Please Choose Image According to Aspected Ratio
-								</Typography>
-							</Box>
-						</Box>
+    return (
+        <Container>
+            <Typography variant='h5' fontWeight='bold'>
+                Products
+            </Typography>
+            <Box sx={{ border: '1px solid', borderColor: 'gray.100', borderRadius: '20px' }}>
+                <Box sx={{ display: 'grid', p: 3, gap: 3, gridTemplateColumns: 'repeat(12,1fr)' }}>
+                    <Box gridColumn={{ md: 'span 4', xs: 'span 12' }}>
+                        <Box
+                            sx={{
+                                border: '1px solid',
+                                borderColor: 'gray.100',
+                                borderRadius: '20px',
+                                aspectRatio: '4 / 3',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <Box>
+                                <Typography variant='h3' fontWeight='bold' textAlign='center' color='gray.500'>
+                                    765 x 850
+                                </Typography>
+                                <Typography variant='caption' color='gray.100'>
+                                    Please Choose Image According to Aspected Ratio
+                                </Typography>
+                            </Box>
+                        </Box>
 
-						<Box sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: 2, paddingTop: 3 }}>
-							<Box
-								sx={{
-									aspectRatio: '4/3',
-									backgroundColor: 'gray.50',
-									display: 'flex',
-									alignItems: 'center',
-									justifyContent: 'center',
-									border: '1px solid',
-									borderColor: 'gray.100',
-									borderRadius: '16px',
-								}}
-							>
-								<Typography variant='body2' fontWeight='bold' textAlign='center' color='gray.500'>
-									750 x 850
-								</Typography>
-							</Box>
-							<Box
-								sx={{
-									border: '1px solid',
-									borderColor: 'gray.100',
-									borderRadius: '16px',
-									backgroundColor: 'gray.50',
-									aspectRatio: '4/3',
-									display: 'flex',
-									alignItems: 'center',
-									justifyContent: 'center',
-								}}
-							>
-								<Typography variant='body2' fontWeight='bold' textAlign='center' color='gray.500'>
-									750 x 850
-								</Typography>
-							</Box>
-							<Box
-								sx={{
-									aspectRatio: '4/3',
-									display: 'flex',
-									alignItems: 'center',
-									justifyContent: 'center',
-									backgroundColor: 'gray.50',
-									border: '1px solid',
-									borderColor: 'gray.100',
-									borderRadius: '16px',
-								}}
-							>
-								<Typography variant='body2' fontWeight='bold' textAlign='center' color='gray.500'>
-									750 x 850
-								</Typography>
-							</Box>
-							<Box
-								sx={{
-									aspectRatio: '4/3',
-									display: 'flex',
-									alignItems: 'center',
-									backgroundColor: 'gray.50',
-									justifyContent: 'center',
-									border: '1px solid',
-									borderColor: 'gray.100',
-									borderRadius: '16px',
-								}}
-							>
-								<Typography variant='body2' fontWeight='bold' textAlign='center' color='gray.500'>
-									750 x 850
-								</Typography>
-							</Box>
-							<Box
-								sx={{
-									aspectRatio: '4/3',
-									display: 'flex',
-									alignItems: 'center',
-									backgroundColor: 'gray.50',
-									justifyContent: 'center',
-									border: '1px solid',
-									borderColor: 'gray.100',
-									borderRadius: '16px',
-								}}
-							>
-								<Typography variant='body2' fontWeight='bold' textAlign='center' color='gray.500'>
-									750 x 850
-								</Typography>
-							</Box>
-						</Box>
-					</Box>
-					<Stack gridColumn={{ md: 'span 8', xs: 'span 12' }} sx={{ width: '100%', gap: '20px' }}>
-						<TextField required id='outlined-required' label='PRODUCT NAME' sx={{ width: '50%' }} />
-						<TextField
-							required
-							id='outlined-required'
-							label='SELECT CATEGORIES'
-							sx={{ width: '50%' }}
-						/>
-						<TextField required id='outlined-required' label='SLUG' />
-						<TextField required id='outlined' label='SORT DESCRIPTION' />
-						<Box>
-							<Typography variant='body1' color='initial'>
-								colors
-							</Typography>
-							<Stack flexDirection='row' gap='8px'>
-								<Box sx={{ width: '20px', height: '20px', backgroundColor: 'red.600' }} />
-								<Box sx={{ width: '20px', height: '20px', backgroundColor: 'red.600' }} />
-								<Box sx={{ width: '20px', height: '20px', backgroundColor: 'red.600' }} />
-								<Box sx={{ width: '20px', height: '20px', backgroundColor: 'red.600' }} />
-							</Stack>
-						</Box>
-					</Stack>
-				</Box>
-			</Box>
-		</Container>
-	);
+                        <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: 2, paddingTop: 3 }}>
+                            <Box
+                                sx={{
+                                    aspectRatio: '4/3',
+                                    backgroundColor: 'gray.50',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    border: '1px solid',
+                                    borderColor: 'gray.100',
+                                    borderRadius: '16px',
+                                }}
+                            >
+                                <Typography variant='body2' fontWeight='bold' textAlign='center' color='gray.500'>
+                                    750 x 850
+                                </Typography>
+                            </Box>
+                            <Box
+                                sx={{
+                                    border: '1px solid',
+                                    borderColor: 'gray.100',
+                                    borderRadius: '16px',
+                                    backgroundColor: 'gray.50',
+                                    aspectRatio: '4/3',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <Typography variant='body2' fontWeight='bold' textAlign='center' color='gray.500'>
+                                    750 x 850
+                                </Typography>
+                            </Box>
+                            <Box
+                                sx={{
+                                    aspectRatio: '4/3',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    backgroundColor: 'gray.50',
+                                    border: '1px solid',
+                                    borderColor: 'gray.100',
+                                    borderRadius: '16px',
+                                }}
+                            >
+                                <Typography variant='body2' fontWeight='bold' textAlign='center' color='gray.500'>
+                                    750 x 850
+                                </Typography>
+                            </Box>
+                            <Box
+                                sx={{
+                                    aspectRatio: '4/3',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    backgroundColor: 'gray.50',
+                                    justifyContent: 'center',
+                                    border: '1px solid',
+                                    borderColor: 'gray.100',
+                                    borderRadius: '16px',
+                                }}
+                            >
+                                <Typography variant='body2' fontWeight='bold' textAlign='center' color='gray.500'>
+                                    750 x 850
+                                </Typography>
+                            </Box>
+                            <Box
+                                sx={{
+                                    aspectRatio: '4/3',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    backgroundColor: 'gray.50',
+                                    justifyContent: 'center',
+                                    border: '1px solid',
+                                    borderColor: 'gray.100',
+                                    borderRadius: '16px',
+                                }}
+                            >
+                                <Typography variant='body2' fontWeight='bold' textAlign='center' color='gray.500'>
+                                    750 x 850
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Box>
+                    <Stack gridColumn={{ md: 'span 8', xs: 'span 12' }} sx={{ width: '100%', gap: '20px' }}>
+                        <TextField required id='outlined-required' label='PRODUCT NAME' sx={{ width: '50%' }} />
+                        <TextField
+                            required
+                            id='outlined-required'
+                            label='SELECT CATEGORIES'
+                            sx={{ width: '50%' }}
+                        />
+                        <TextField required id='outlined-required' label='SLUG' />
+                        <TextField required id='outlined' label='SORT DESCRIPTION' />
+                        <Stack justifyContent='space-between' flexDirection='row' alignItems='center'>
+                            <Box>
+                                <Typography variant='body1' fontSize={20} color='gray.300'>
+                                    COLORS
+                                </Typography>
+                                <Stack flexDirection='row' gap='8px'>
+                                    <Box sx={{ width: '20px', height: '20px', backgroundColor: 'red.600' }} />
+                                    <Box sx={{ width: '20px', height: '20px', backgroundColor: 'red.600' }} />
+                                    <Box sx={{ width: '20px', height: '20px', backgroundColor: 'red.600' }} />
+                                    <Box sx={{ width: '20px', height: '20px', backgroundColor: 'red.600' }} />
+                                </Stack>
+                            </Box>
+                            <Box>
+                                <Typography variant='body1' fontSize={20} color='gray.300'>
+                                    SIZE
+                                </Typography>
+                                <Stack flexDirection='row'>
+                                    <Typography variant='body1' color='gray.300'><Checkbox style={{ padding: '0px' }} defaultChecked={false} size='small' />S</Typography>
+                                    <Typography variant='body1' color='gray.300'><Checkbox style={{ padding: '0px' }} defaultChecked={false} size='small' />L</Typography>
+                                    <Typography variant='body1' color='gray.300'><Checkbox style={{ padding: '0px' }} defaultChecked={false} size='small' />XL</Typography>
+                                    <Typography variant='body1' color='gray.300'><Checkbox style={{ padding: '0px' }} defaultChecked={false} size='small' />XXL</Typography>
+
+                                </Stack>
+                            </Box>
+                        </Stack>
+                        <Stack flexDirection='row' gap={3}>
+                            <TextField required id='outlined-required' label='PRICE' sx={{ width: '50%' }} />
+                            <TextField
+                                required
+                                id='outlined-required'
+                                label='QUANTITY'
+                                sx={{ width: '50%' }}
+                            />
+                        </Stack>
+                        <TextField
+                            id="outlined-multiline-static"
+                            label="Multiline"
+                            multiline
+                            minRows={4}
+                            sx={{ height: '10px' }}
+                            defaultValue="Default Value"
+                        />
+
+                    </Stack>
+                </Box>
+            </Box>
+        </Container>
+    );
 };
 
 export default AddProduct;
