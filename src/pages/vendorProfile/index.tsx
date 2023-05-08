845c56f28230cd899634944aa984a28b16b691b4
abidali671 <abid.saeed.ali92@gmail.com>
2023-05-08 14:46:48 +0500

    hot fix

--- src/pages/vendorProfile/index.tsx
+++ src/pages/vendorProfile/index.tsx
@@ -2,75 +2,87 @@ import React from 'react';
 import { Container, Breadcrumbs } from '@components';
 import { Box, Typography, Button, Stack } from '@mui/material';
 import { useParams } from 'react-router-dom';
-import * as styles from './vendorProfile.styes';
+import * as styles from './vendorProfile.styles';
 import ProfileImage from '@assests/profile.png';
 
 const VendorProfile = () => {
-  const { id } = useParams();
-  return (
-    <Container>
-      <Typography variant='h5' fontWeight='bold'>
-        Vendor Profile
-      </Typography>
-      <Breadcrumbs />
+	const { id } = useParams();
 
-      <Box sx={styles.gridBox}>
-        <Box sx={{ gridColumn: 'span 3' }}>
-          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', p: 3, borderRight: '1px solid', borderColor: 'gray.100', }} gap={3}>
-            <Box textAlign='center'>
-              <Box
-                component='img'
-                src={ProfileImage}
-                sx={{ width: '110px', height: '100px', borderRadius: '50%' }}
-              />
-              <Typography variant='body1' color='initial' fontSize={16} fontWeight={600}>
-                Riley Reid
-              </Typography>
-              <Typography variant='body1' color='initial' fontSize={16}>
-                rileyreid@gmail.com
-              </Typography>
-            </Box>
+	return (
+		<Container>
+			<Typography variant='h5' fontWeight='bold'>
+				Vendor Profile
+			</Typography>
+			<Breadcrumbs />
 
-            <Button
-              variant='contained'
-              size='medium'
-              sx={{ backgroundColor: 'blue.400', borderRadius: '20px' }}
-            >
-              Follow
-            </Button>
+			<Box sx={styles.gridBox}>
+				<Box sx={{ gridColumn: 'span 3' }}>
+					<Box
+						sx={{
+							display: 'flex',
+							flexDirection: 'column',
+							justifyContent: 'center',
+							alignItems: 'center',
+							p: 3,
+							borderRight: '1px solid',
+							borderColor: 'gray.100',
+						}}
+						gap={3}
+					>
+						<Box textAlign='center'>
+							<Box
+								component='img'
+								src={ProfileImage}
+								sx={{ width: '110px', height: '100px', borderRadius: '50%' }}
+							/>
+							<Typography variant='body1' color='initial' fontSize={16} fontWeight={600}>
+								Riley Reid
+							</Typography>
+							<Typography variant='body1' color='initial' fontSize={16}>
+								rileyreid@gmail.com
+							</Typography>
+						</Box>
 
-            <Stack gap={2} textAlign='center' flexDirection='row' justifyContent='space-around'>
-              <Box>
-                <Typography variant='body1' color='gray.400' fontWeight={600} fontSize={16}>
-                  1733
-                </Typography>
-                <Typography variant='body1' color='gray.300' fontSize={16}>
-                  Friends
-                </Typography>
-              </Box>
-              <Box>
-                <Typography variant='body1' color='gray.400' fontWeight={600} fontSize={16}>
-                  1733
-                </Typography>
-                <Typography variant='body1' color='gray.300' fontSize={16}>
-                  Followers
-                </Typography>
-              </Box>
-              <Box>
-                <Typography variant='body1' color='gray.400' fontWeight={600} fontSize={16}>
-                  1733
-                </Typography>
-                <Typography variant='body1' color='gray.300' fontSize={16}>
-                  Following
-                </Typography>
-              </Box>
-            </Stack>
-          </Box>
-        </Box>
-        <Box sx={{ gridColumn: 'span 9' }}>dear</Box>
-      </Box>
-    </Container>
-  );
+						<Button
+							variant='contained'
+							size='medium'
+							sx={{ backgroundColor: 'blue.400', borderRadius: '20px' }}
+						>
+							Follow
+						</Button>
+
+						<Stack gap={2} textAlign='center' flexDirection='row' justifyContent='space-around'>
+							<Box>
+								<Typography variant='body1' color='gray.400' fontWeight={600} fontSize={16}>
+									1733
+								</Typography>
+								<Typography variant='body1' color='gray.300' fontSize={16}>
+									Friends
+								</Typography>
+							</Box>
+							<Box>
+								<Typography variant='body1' color='gray.400' fontWeight={600} fontSize={16}>
+									1733
+								</Typography>
+								<Typography variant='body1' color='gray.300' fontSize={16}>
+									Followers
+								</Typography>
+							</Box>
+							<Box>
+								<Typography variant='body1' color='gray.400' fontWeight={600} fontSize={16}>
+									1733
+								</Typography>
+								<Typography variant='body1' color='gray.300' fontSize={16}>
+									Following
+								</Typography>
+							</Box>
+						</Stack>
+					</Box>
+				</Box>
+				<Box sx={{ gridColumn: 'span 9' }}>dear</Box>
+			</Box>
+		</Container>
+	);
 };
 
 export default VendorProfile;
---
+++ src/pages/vendorProfile/vendorProfile.styles.ts
@@ -0,0 +1,8 @@
+export const gridBox = {
+	display: 'grid',
+	gap: 2,
+	gridTemplateColumns: 'repeat(12,1fr)',
+	border: '1px solid',
+	borderColor: 'gray.100',
+	borderRadius: '10px',
+};
