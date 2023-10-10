0511063e6e94f9727ca432acc87814acdab57541
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2023-10-10 15:14:45 -0700

    fix style of add category

--- src/pages/addCategory/addCategory.styles.ts
+++ src/pages/addCategory/addCategory.styles.ts
@@ -5,7 +5,9 @@ export const mainForm = {
     flexDirection: 'column',
     gap: '8px',
 }
-
+export const BoxLeft = {
+    display: 'flex', flexWrap: { xs: 'wrap', md: 'nowrap', }
+}
 export const smallBoxes = {
 
     backgroundColor: 'gray.50',
@@ -15,7 +17,8 @@ export const smallBoxes = {
     border: '1px solid dotted',
     borderColor: 'gray.100',
     borderRadius: '16px',
-    width: '330px',
+    width: { xs: '450px', md: '300px' },
+
     height: '165px'
 
 };
