f9a9a9d8ca5fd51386e0086ececed848312373b6
abidali671 <abid.saeed.ali92@gmail.com>
2023-10-17 03:17:46 +0500

    fix: add category flow

--- src/pages/addCategory/addCategory.hook.ts
+++ src/pages/addCategory/addCategory.hook.ts
@@ -1,41 +1,66 @@
 import { useFormik } from 'formik';
+import { useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDropzone } from 'react-dropzone';
 
 import API from 'src/axios';
 
 const initialValues = {
-    name: '',
-    slug: '',
-    thumbnail: '',
-    thumbnailFile: '',
-    description: '',
+	name: '',
+	slug: '',
+	thumbnail: '',
+	thumbnailFile: '',
+	description: '',
 };
-const AddToCategory = () => {
+const useAddCategory = () => {
+	const navigate = useNavigate();
 
-    const navigate = useNavigate()
+	const handleSubmit = async (values: typeof initialValues) => {
+		try {
+			const { thumbnailFile, ...data } = values;
+			const res = await API.post('/api/category', data);
+			await API.post(
+				`/api/category/${res.data.id}/thumbnail`,
+				{ file: thumbnailFile },
+				{
+					headers: {
+						'Content-Type': 'multipart/form-data',
+					},
+				},
+			);
 
-    const handleSubmit = async (values: typeof initialValues) => {
-        try {
-            const { thumbnailFile, ...data } = values
-            const res = await API.post('/api/category', data);
-            API.post(`/api/category/${res.data.id}/thumbnail`, { file: thumbnailFile })
-            console.log(res, '===>')
-            navigate('/category')
+			navigate('/category');
+		} catch (error) {
+			formik.setErrors(error.response.data);
+			console.log(error.response.data);
+		}
+	};
 
+	const formik = useFormik({
+		initialValues: initialValues,
+		onSubmit: handleSubmit,
+	});
 
-        } catch (error) {
-            formik.setErrors(error.response.data);
-            console.log(error.response.data)
-        }
+	const onDrop = useCallback((acceptedFiles: Blob[]) => {
+		formik.setFieldValue('thumbnail', URL.createObjectURL(acceptedFiles[0]));
+		formik.setFieldValue('thumbnailFile', acceptedFiles[0]);
+	}, []);
 
-    };
+	const { getRootProps, getInputProps } = useDropzone({
+		onDrop,
+		multiple: false,
+	});
 
-    const formik = useFormik({
-        initialValues: initialValues,
-        onSubmit: handleSubmit,
-    });
+	const updateSlug = (categoryTitle: string) => {
+		const updatedSlug = categoryTitle.toLowerCase().replace(/ /g, '-');
+		formik.setFieldValue('slug', updatedSlug);
+	};
 
-    return { formik };
+	useEffect(() => {
+		updateSlug(formik.values.name);
+	}, [formik.values.name]);
+
+	return { formik, getRootProps, getInputProps };
 };
 
-export default AddToCategory;
+export default useAddCategory;
--- src/pages/addCategory/index.tsx
+++ src/pages/addCategory/index.tsx
@@ -1,137 +1,111 @@
+import React from 'react';
+import useAddCategory from './addCategory.hook';
 import { Container, Card, Breadcrumbs, FormikTextField } from '@components';
 import { BoxLeft, smallBoxes } from './addCategory.styles';
-import { useDropzone } from 'react-dropzone';
-import { Typography, Grid, Box, FormControl, Button } from '@mui/material';
-import { useCallback, useEffect, useState } from 'react';
-import AddToCategory from './addCategory.hook';
+import { Typography, Grid, Box, FormControl } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
-import API from 'src/axios';
 
-const AddCategory = () => {
-    const { formik } = AddToCategory();
-    const onDrop = useCallback(
+const AddCategory: React.FC = () => {
+	const { formik, getRootProps, getInputProps } = useAddCategory();
 
-        (acceptedFiles: Blob[]) => {
-            formik.setFieldValue('thumbnail', URL.createObjectURL(acceptedFiles[0]))
+	return (
+		<Container>
+			<Box>
+				<Typography variant='h5' fontWeight='bold'>
+					Category
+				</Typography>
+				<Breadcrumbs />
+			</Box>
+			<Grid container columns={12} spacing={4}>
+				<Grid item xs={12} md={12}>
+					<Card title='Add New Category'>
+						<Box
+							component='form'
+							onSubmit={formik.handleSubmit}
+							sx={{
+								display: 'flex',
+								justifyContent: 'center',
+								padding: '16px 24px',
+								flexDirection: 'column',
+								gap: '8px',
+							}}
+						>
+							<Box gap={2} sx={BoxLeft}>
+								<Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+									<FormControl>
+										<Typography fontWeight='bold' variant='body1'>
+											Name
+										</Typography>
 
-            formik.setFieldValue('thumbnailFile', acceptedFiles[0])
-        },
-        [],
-    );
+										<FormikTextField name='name' {...formik} placeholder='Please enter your name' />
+									</FormControl>
+									<FormControl>
+										<Typography fontWeight='bold' variant='body1'>
+											Slug
+										</Typography>
+										<FormikTextField {...formik} name='slug' placeholder='Please enter your slug' />
+									</FormControl>
+									<Typography variant='caption'>
+										The “slug” is the URL-friendly version of the name. It is usually all lowercase
+										and contains only letters, numbers, and hyphens
+									</Typography>
+								</Box>
+								<Box>
+									<Typography fontWeight='bold' variant='body1'>
+										Thumbnail
+									</Typography>
+									<Box sx={smallBoxes} {...getRootProps()}>
+										<input {...getInputProps()} type='file' />
+										{formik.values.thumbnail ? (
+											<Box
+												component='img'
+												src={formik.values.thumbnail}
+												sx={{ width: '100%', height: '100%', objectFit: 'contain' }}
+											/>
+										) : (
+											<Box textAlign='center'>
+												<Typography variant='body1' textAlign='center' color='gray.500'>
+													Upload Imag
+												</Typography>
+											</Box>
+										)}
+									</Box>
+								</Box>
+							</Box>
+							<FormControl>
+								<Typography variant='body1' fontWeight='bold'>
+									Description
+								</Typography>
 
-    const { getRootProps, getInputProps } = useDropzone({
-        onDrop,
-        multiple: false,
-    });
-
-    //  add slug
-    const updateSlug = (categoryTitle) => {
-        const updatedSlug = categoryTitle.toLowerCase().replace(/ /g, '-');
-        formik.setFieldValue('slug', updatedSlug);
-    };
-
-    useEffect(() => {
-        updateSlug(formik.values.name)
-    }, [formik.values.name])
-    return (
-        <Container>
-            <Box>
-                <Typography variant='h5' fontWeight='bold'>
-                    Category
-                </Typography>
-                <Breadcrumbs />
-            </Box>
-            <Grid container columns={12} spacing={4}>
-                <Grid item xs={12} md={12}>
-                    <Card title='Add New Category'>
-                        <Box
-                            component='form'
-                            onSubmit={formik.handleSubmit}
-                            sx={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                                padding: '16px 24px',
-                                flexDirection: 'column',
-                                gap: '8px',
-                            }}
-                        >
-                            <Box gap={2} sx={BoxLeft}>
-                                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-                                    <FormControl>
-                                        <Typography fontWeight='bold' variant='body1'>
-                                            Name
-                                        </Typography>
-
-                                        <FormikTextField name='name'
-                                            {...formik} placeholder='Please enter your name' />
-                                    </FormControl>
-                                    <FormControl>
-                                        <Typography fontWeight='bold' variant='body1'>
-                                            Slug
-                                        </Typography>
-                                        <FormikTextField  {...formik} name='slug' placeholder='Please enter your slug' />
-                                    </FormControl>
-                                    <Typography variant='caption'>
-                                        The “slug” is the URL-friendly version of the name. It is usually all lowercase
-                                        and contains only letters, numbers, and hyphens
-                                    </Typography>
-                                </Box>
-                                <Box>
-                                    <Typography fontWeight='bold' variant='body1'>
-                                        Thumbnail
-                                    </Typography>
-                                    <Box sx={smallBoxes} {...getRootProps()}>
-                                        <input {...getInputProps()} type='file' />
-                                        {formik.values.thumbnail ? (
-                                            <Box
-                                                component='img'
-                                                src={formik.values.thumbnail}
-                                                sx={{ width: '100%', height: '100%', objectFit: 'contain' }}
-                                            />
-                                        ) : (
-                                            <Box textAlign='center'>
-                                                <Typography variant='body1' textAlign='center' color='gray.500'>
-                                                    Upload Imag
-                                                </Typography>
-                                            </Box>
-                                        )}
-                                    </Box>
-                                </Box>
-                            </Box>
-                            <FormControl>
-                                <Typography variant='body1' fontWeight='bold'>
-                                    Description
-                                </Typography>
-
-                                <FormikTextField
-                                    sx={{ '.MuiOutlinedInput-root': { height: 'auto' } }}
-                                    multiline
-                                    rows={5}
-                                    name='description'
-                                    {...formik}
-                                />
-                            </FormControl>
-                            <FormControl>
-                                <LoadingButton
-                                    variant='contained'
-                                    sx={{
-                                        backgroundColor: 'blue.500',
-                                        color: 'white',
-                                        width: '120px',
-                                        marginTop: '14px',
-                                    }}
-                                    loading={formik.isSubmitting}
-                                    type='submit'
-                                >
-                                    Submit
-                                </LoadingButton>
-                            </FormControl>
-                        </Box>
-                    </Card>
-                </Grid>
-            </Grid>
-        </Container>
-    );
+								<FormikTextField
+									sx={{ '.MuiOutlinedInput-root': { height: 'auto' } }}
+									multiline
+									rows={5}
+									name='description'
+									{...formik}
+								/>
+							</FormControl>
+							<FormControl>
+								<LoadingButton
+									variant='contained'
+									sx={{
+										backgroundColor: 'blue.500',
+										color: 'white',
+										width: '120px',
+										marginTop: '14px',
+									}}
+									loading={formik.isSubmitting}
+									type='submit'
+								>
+									Submit
+								</LoadingButton>
+							</FormControl>
+						</Box>
+					</Card>
+				</Grid>
+			</Grid>
+		</Container>
+	);
 };
 
 export default AddCategory;
