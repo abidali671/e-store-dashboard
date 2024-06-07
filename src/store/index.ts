5fc002ecee409ae703f34ec2ef2d9b4a5cec71a5
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2024-06-07 14:52:52 -0700

    add persist for user auth

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dropDowns from 'src/features/constent/dropdowns.slice';
 import authReducer from '../features/auth/auth.slice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+const persistConfig = {
+	key: 'auth',
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
 	reducer: {
-		auth: authReducer,
+		auth: persistedReducer,
 		dropdowns: dropDowns,
 	},
 });
 
+
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
