3b8f2e3e9b0cc368f6ecdea73b7c7c33a50ac891
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2024-06-10 11:53:06 -0700

    add persist login

--- src/pages/categories/ActionButton.tsx
+++ src/pages/categories/ActionButton.tsx
@@ -1,8 +1,4 @@
-import {
-    Box, IconButton,
-    Menu,
-    MenuItem
-} from '@mui/material';
+import { Box, IconButton, Menu, MenuItem } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { MoreOption } from '@assests/icons';
 import { useState } from 'react';
@@ -10,36 +6,36 @@ import API from 'src/axios';
 import { toast } from 'react-toastify';
 
 export const ActionButton = ({ item }) => {
-    const [anchorEl, setAnchorEl] = useState(null);
-    const navigate = useNavigate();
-    const handleEdit = () => {
-        navigate(`/category/${item.slug}`);
-    };
+	const [anchorEl, setAnchorEl] = useState(null);
+	const navigate = useNavigate();
+	const handleEdit = () => {
+		navigate(`/category/${item.slug}`);
+	};
 
-    const handleDelete = async () => {
-        try {
-            const res = await API.delete(`/api/category/${item.slug}`);
-            toast.success(res.data.msg);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+	const handleDelete = async () => {
+		try {
+			const res = await API.delete(`/api/category/${item.slug}`);
+			toast.success(res.data.msg);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-    return (
-        <Box>
-            <IconButton onClick={handleClick}>
-                <MoreOption style={{ height: '20px', width: '20px' }} />
-            </IconButton>
-            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
-                <MenuItem onClick={handleEdit}>Edit</MenuItem>
-                <MenuItem onClick={handleDelete}>Delete</MenuItem>
-            </Menu>
-        </Box>
-    );
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	return (
+		<Box>
+			<IconButton onClick={handleClick}>
+				<MoreOption style={{ height: '20px', width: '20px' }} />
+			</IconButton>
+			<Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+				<MenuItem onClick={handleEdit}>Edit</MenuItem>
+				<MenuItem onClick={handleDelete}>Delete</MenuItem>
+			</Menu>
+		</Box>
+	);
 };
